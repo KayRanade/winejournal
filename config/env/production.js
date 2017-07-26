c294f479ba23f8a8beb7ea2038382a1e11ff41d3
Kshama Ranade <kshamaranade@Kshamas-MacBook-Air.local>
2017-07-26 12:18:42 -0400

    no ssl

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  secure: true,
+  secure: false,
   port: process.env.PORT || 8443,
   db: {
     uri: "mongodb://admin:admin@ds125623.mlab.com:25623/winejournal" || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
